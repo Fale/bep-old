15cc771ed0966e8e0bbe7f954a8f0b6885b9bd93
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-08-31 19:25:24 +0200

    Add login and logout functions for all pages

--- businesscanvas/urls.py
+++ businesscanvas/urls.py
@@ -9,5 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login$', 'django.contrib.auth.views.login'),
+    url(r'^logout$', 'canvas.views.user_logout'),
     url(r'^', include('canvas.urls')),
 )
