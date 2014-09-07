5f9888a8ab1236f9f264117d3b1eff17a78b0ea0
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-09-07 15:50:25 +0200

    Move to a more url agnostic linking

--- businesscanvas/urls.py
+++ businesscanvas/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login$', 'django.contrib.auth.views.login'),
-    url(r'^logout$', 'canvas.views.user_logout'),
+    url(r'^logout$', 'canvas.views.logout.user_logout'),
     url(r'^', include('canvas.urls')),
 )
